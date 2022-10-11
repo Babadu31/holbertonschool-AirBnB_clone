29219879d2c5f47ff09ad743ac753228d69385ec
Babadu31 <bastienmiss31@gmail.com>
2022-10-11 06:29:04 -0700

    15:13_11/10/2022 Main merging task 3 from DEVOP

--- base_model.py
+++ base_model.py
@@ -18,13 +18,19 @@ class BaseModel:
         """
         string
         """
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """
+        un truc pértinent
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        dict_obj = self.__dict__
+        """"
+        un second truc pertinent
+        """
+        dict_obj = self.__dict__.copy()
         dict_obj["__class__"] = self.__class__.__name__
         self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
