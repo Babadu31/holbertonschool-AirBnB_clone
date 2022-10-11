2648c4af349a241c2f3d9fb097ccb7870a3370a0
Babadu31 <bastienmiss31@gmail.com>
2022-10-11 07:37:59 -0700

    15:57-11/10/22-test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,9 +2,31 @@
 
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
     
-    def TestSave(self):
-        b = BaseModel()
-        self.assertNotEqual(b.updated_at, b.created_at)
+    def TestSave(self):         # ne marches pas 
+        b0 = BaseModel()
+        self.assertEqual(type(b0. id), str)
+        self.assertEqual(type(b0.created_at), datetime)
+        self.assertEqual(type(b0.updated_at), datetime)
+
+
+    def test_id(self):          # marches
+        self.assertEqual(str, type(BaseModel().id))
+
+    def test_str(self):             # marches 
+        b1 = BaseModel()
+        self.assertEqual(type(str(b1)), str)
+    
+    def test_to_dict(self):     # marches
+        b2 = BaseModel()
+        self.assertTrue(dict, type(b2.to_dict))
+    
+    def test_save(self):           # marches
+        b3 = BaseModel()
+        update = b3.updated_at
+        b3.save()
+        self.assertNotEqual(update,b3.updated_at)
+    
