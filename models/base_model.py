ed177609375c36709e582379364707b3a8489cbf
Babadu31 <bastienmiss31@gmail.com>
2022-10-11 06:04:28 -0700

    15-03_11-10-2022_Devops changes

--- models/base_model.py
+++ models/base_model.py
@@ -10,8 +10,8 @@ class BaseModel:
     """
     def __init__(self):
         self.id = str(uuid4())
-        created_at = datetime.now()
-        updated_at = datetime.now()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -20,12 +20,12 @@ class BaseModel:
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
-        updated_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         dict_obj = self.__dict__
         dict_obj["__class__"] = self.__class__.__name__
-        self.created_at = created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         return (dict_obj)
         
