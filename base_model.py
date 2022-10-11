0c98b66bc1537b696daef3d247029a02f1f3961f
Babadu31 <bastienmiss31@gmail.com>
2022-10-11 05:59:50 -0700

    14:59-11/10/22-test.base.final

---
+++ base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+    """
+    Base class
+    """
+    def __init__(self):
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        self.id = str(uuid4())
+
+
+    def __str__(self):
+        """
+        string
+        """
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict_obj = self.__dict__
+        dict_obj["__class__"] = self.__class__.__name__
+        self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return (dict_obj)
