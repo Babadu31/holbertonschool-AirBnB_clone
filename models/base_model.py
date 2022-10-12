11bc3ff12e49095f6e163bd3d579f8b38ad9d812
Babadu31 <bastienmiss31@gmail.com>
2022-10-12 06:13:20 -0700

    15:11-12/10/22-basemodel/4

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,44 @@
 #!/usr/bin/python3
 
-from uuid import uuid4
 from datetime import datetime
+from uuid import uuid4
 
 
 class BaseModel:
     """
     Base class
     """
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            self.id = str(uuid4())
+
 
     def __str__(self):
         """
         string
         """
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """
+        un truc pértinent
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        dict_obj = self.__dict__.copy()
-        dict_obj["__class__"] = self.__class__.__name__
-        self.created_at = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        return (dict_obj)
-      
+        dictionary = {"__class__": self.__class__.__name__}
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                dictionary[key] = value.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                dictionary[key] = value
+        return dictionary
