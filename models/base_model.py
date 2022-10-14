5eb4ba5bb57a2cc688f5a8e3f262f8e3b8491de9
guenoel <guenooel@hotmail.com>
2022-10-14 14:51:59 -0700

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,36 +1,31 @@
-#!/usr/bin/python3
-
-from uuid import uuid4
 from datetime import datetime
-
+from models import storage
+import uuid
 
 class BaseModel:
-    """
-    Base class
-    """
-    def __init__(self):
-        self.id = str(uuid4())
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for k, v in kwargs.items():
+                if "__class__" != k:
+                    if k in ("created_at", "updated_at"):
+                        setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, k, v)
+            return
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
+        self.updated_at = self.created_at
+        storage.new(self)
+    
     def __str__(self):
-        """
-        string
-        """
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-
+        
     def save(self):
-        """
-        un truc pértinent
-        """
         self.updated_at = datetime.now()
         storage.save()
         
     def to_dict(self):
-        dictionary = {"__class__": self.__class__.__name__}
-        for key, value in self.__dict__.items():
-            if key == "created_at" or key == "updated_at":
-                dictionary[key] = value.strftime("%Y-%m-%dT%H:%M:%S.%f")
-            else:
-                dictionary[key] = value
-        return dictionary
+        self.__dict__['__class__'] = self.__class__.__name__
+        self.__dict__['created_at'] = self.created_at.isoformat()
+        self.__dict__['updated_at'] = self.updated_at.isoformat()
+        return self.__dict__
