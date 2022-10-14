5f4292d82ede06eb363edd190e6e07323eab51d8
guenoel <guenooel@hotmail.com>
2022-10-14 14:23:07 -0700

    task 9

--- models/city.py
+++ models/city.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+
+class City:
+    """city class"""
+
+    state_id = ""
+    name = ""
--- models/review.py
+++ models/review.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+class Review:
+    """class review"""
+
+    place_id = ""
+    user_id = ""
+    text = ""
