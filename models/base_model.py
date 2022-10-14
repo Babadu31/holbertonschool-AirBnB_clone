d736b35e849617dff8027b41373754dca6f2d2bc
Babadu31 <bastienmiss31@gmail.com>
2022-10-14 06:43:10 -0700

    15:42-14/10/22-comments

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+BaseModel class
+"""
+
 
 from datetime import datetime
 from uuid import uuid4
@@ -8,11 +12,13 @@ from models import storage
 class BaseModel:
     """
     defines base model attributes and methods
+    created_at: The datetime at creation
+    updated_at: The datetime of last update
     """
 
     def __init__(self, *args, **kwargs):
         """
-        create à new instance and save the info
+        create à new instance and save the info (Initializes the BaseModel)
         """
         if kwargs:
             for key, value in kwargs.items():
@@ -28,18 +34,21 @@ class BaseModel:
 
     def __str__(self):
         """
-        Change output to a specific format
+        return output to a specific format
         """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
-        un truc pértinent
+        Update updated_at with the current datetime and save it
         """
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
+        """
+        Return a dictionary representation of the BaseModel
+        """
         dictionary = {"__class__": self.__class__.__name__}
         for key, value in self.__dict__.items():
             if key == "created_at" or key == "updated_at":
